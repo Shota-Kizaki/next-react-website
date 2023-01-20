40dac581c25136125ef441a618853b95ffb441c2
Shota-Kizaki <ktc2243030002@edu.kyoto-tech.ac.jp>
2023-01-20 14:13:57 +0900

    111

--- components/contact.js
+++ components/contact.js
@@ -1,10 +1,10 @@
 import styles from 'styles/contact.module.css'
 
-export default function Contact(){
+export default function Contact() {
     return (
         <div className={styles.stack}>
             <h3 className={styles.heading}>Contact</h3>
-            <address> cube@web.mail.address</address>
+            <address>cube@web.mail.address</address>
         </div>
     )
 }
