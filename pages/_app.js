1bf5ef958d062d885d5e5e52bf6d504e97f892be
Shota-Kizaki <ktc2243030002@edu.kyoto-tech.ac.jp>
2023-01-21 22:31:38 +0900

    171

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "styles/globals.css"
 import Layout from "components/layout"
+
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
