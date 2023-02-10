1ddf71224a432a73ae48fc2b6df94784630c27e1
Shota-Kizaki <ktc2243030002@edu.kyoto-tech.ac.jp>
2023-02-10 14:41:32 +0900

    194

--- components/meta.js
+++ components/meta.js
@@ -1,10 +1,21 @@
 import Head from 'next/head'
 
-export default function Meta({ pageTitle }){
+//サイトに関する情報
+import { siteMeta } from 'lib/constants'
+const { siteTitle, siteDesc, siteUrl, siteLocale, siteType, siteIcon } = siteMeta
+
+export default function Meta({ pageTitle, pageDesc }){
+//ページのタイトル
+    const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle
+
+//ページの説明
+    const desc =pageDesc ?? siteDesc
     return(
         <Head>
-            <title>{pageTitle}</title>
-            <meta property='og:title' content={pageTitle} />
+            <title>{title}</title>
+            <meta property='og:title' content={title}/>
+            <meta name='descriotion' content={desc} />
+            <meta property='og:description' content={desc} />
         </Head>
     )
 }
